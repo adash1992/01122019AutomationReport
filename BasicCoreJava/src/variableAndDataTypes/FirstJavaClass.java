b5df9dd5ec969c8d315c8ddfff5bcd1f5409d4cd
amrita dash <dashamrita1992@gmail.com>
2019-12-01 15:24:10 +0530

    commit

---
+++ BasicCoreJava/src/variableAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package variableAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the world of Automation");
+	}
+
+}
