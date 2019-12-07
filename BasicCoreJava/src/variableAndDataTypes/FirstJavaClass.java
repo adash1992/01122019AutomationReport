75ed7f7bf9c7b54390e9ac0a74d02f3fde39cb71
amrita dash <dashamrita1992@gmail.com>
2019-12-07 15:36:41 +0530

    modified exist file class

--- BasicCoreJava/src/variableAndDataTypes/FirstJavaClass.java
+++ BasicCoreJava/src/variableAndDataTypes/FirstJavaClass.java
@@ -3,8 +3,9 @@ package variableAndDataTypes;
 public class FirstJavaClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Welcome to the world of Automation");
+		System.out.println("test");
+
 	}
 
 }
